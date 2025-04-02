f15dff61c3ebaa5e81a6373cb7d97a83ec343761
Cesar Lobo <cesar.lobo@firstleaf.com>
2025-04-02 00:39:47 -0300

    improve ci/cd

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,21 +18,24 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 6 : undefined,
+  workers: process.env.CI ? 8 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['list'],
+    ['html']
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     //baseURL: 'https://www.booking.com/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'retain-on-failure',
+    trace: process.env.CI ? 'retain-on-failure' : 'retain-on-failure',
     video: process.env.CI
-    ? { mode: 'retain-on-failure', size: { width: 640, height: 480 } }
-    : { mode: 'retain-on-failure', size: { width: 640, height: 480 } }
+      ? { mode: 'off' }
+      : { mode: 'retain-on-failure', size: { width: 640, height: 480 } }
   },
 
   /* Configure projects for major browsers */
